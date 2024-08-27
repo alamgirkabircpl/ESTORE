6153c79111a483f499653159b1694c0591f56f85
Alamgir Kabir <alamgir.kabir@projukti-bd.com>
2024-08-27 18:32:28 +0600

    test

--- tracko.client/src/app/app.module.ts
+++ tracko.client/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './website/navbar/navbar.component';
+import { HeaderComponent } from './website/header/header.component';
+import { HomeComponent } from './website/home/home.component';
+import { FooterComponent } from './website/footer/footer.component';
+import { AccessoriesComponent } from './website/accessories/accessories.component';
+import { AboutComponent } from './website/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    AccessoriesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
